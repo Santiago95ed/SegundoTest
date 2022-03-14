308b84e5202ad6af5d0eac6be03582a97531d83b
Santiago95ed <santiagoated@hotmail.com>
2022-03-14 18:55:53 +1100

    Huy

---
+++ ICTPRG520/ICTPRG520/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ICTPRG520.MvcApplication" Language="C#" %>
---
+++ ICTPRG520/ICTPRG520/obj/Release/Package/PackageTmp/Scripts/mainSite.js
@@ -0,0 +1,18 @@
+﻿
+$(document).ready(function () {
+  
+    $('.rateCalculator').keyup(function () {
+        var hoursWorked = $(this).val();
+        var rate = $(this).data("rate");
+        var total = hoursWorked * rate;
+        $("#amount" + $(this).data("id")).text(total); 
+    });
+
+ 
+ 
+
+});
+
+
+
+ 
